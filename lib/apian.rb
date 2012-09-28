749409f4a3a9416bc886afbf53ddc614361a9591
Mike Butsko <butsko.7@osu.edu>
2012-09-28 10:16:38 -0400

    Updates

--- lib/apian.rb
+++ lib/apian.rb
@@ -1,23 +1,38 @@
 require "apian/version"
 
 module Apian
-  include ActiveSupport::Concern
+  module ModelExtensions
+    extend ActiveSupport::Concern
 
-  included do
-    before_validation :randomize_key
-    validates_presence_of :key, :name
-    validates_uniqueness_of :key, :name
-  end
+    included do
+      before_validation :randomize_key
+      validates_presence_of :key, :name
+      validates_uniqueness_of :key, :name
+    end
 
-  def randomize_key
-    self.key = SecureRandom.hex(16)
-  end
+    def randomize_key
+      self.key = SecureRandom.hex(16)
+    end
 
-  module ClassMethods
-    def lookup_id_for(api_key)
-      Rails.cache.fetch "service_id_for #{api_key}" do
-        find_by_key(api_key).try(:id)
+    module ClassMethods
+      def lookup_id_for(api_key)
+        Rails.cache.fetch "service_id_for #{api_key}" do
+          find_by_key(api_key).try(:id)
+        end
       end
     end
   end
+
+  module ControllerExtensions
+    def api_key
+      key = request.headers["X-API-Key"]
+      raise "API Key not provided" if key.blank?
+    end
+    def forbidden
+      self.status = 403
+      self.response_body = "API Key Not Found"
+    end
+  end
+
+
 end
